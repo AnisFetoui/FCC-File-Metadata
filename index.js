5c44b7dcf6784ac663eea3f05518a91b1bdb8ac1
Replit user <>
2023-12-27 16:17:05 +0100

    i got 5/4 of challenges

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
+const upload = multer();
 var app = express();
 
 app.use(cors());
@@ -9,6 +10,17 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse",
+         upload.single('upfile'),
+         (req,res)=>{
+  const file = req.file;
+  res.json({name:file.originalname,
+            type:file.mimetype,
+            size:file.size
+           }
+    );
 });
 
 
